73a57df4327e874f4168010e2d35268815c620b7
TimSesel <tim.sesel@student.um.si>
2024-05-15 14:31:24 +0200

    Added modules + Router setup
    Added modules for icons and routing.
    
    Routed up components and added header.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserContext } from "./userContext";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Logout from "./components/Logout";
+import Map from "./components/Map";
+import Box from '@mui/joy/Box';
 
 function App() {
+  const [user, setUser] = useState(localStorage.user ? JSON.parse(localStorage.user) : null);
+  const updateUserData = (userInfo) => {
+    localStorage.setItem("user", JSON.stringify(userInfo));
+    setUser(userInfo);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{
+        user: user,
+        setUserContext: updateUserData
+      }}>
+        <div className="App">
+          <Header title="NOISE POLLUTION"></Header>
+          <Box component="section" sx={{ m: '2%', p: 2, bgcolor: 'neutral.100', borderRadius: 16 }}>
+            <Routes>
+              <Route path="/" exact element={<Map />}></Route>
+              <Route path="/login" exact element={<Login />}></Route>
+              <Route path="/register" element={<Register />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/logout" element={<Logout />}></Route>
+            </Routes>
+          </Box>
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,60 @@
+import { UserContext } from "../userContext";
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import List from '@mui/joy/List';
+import ListDivider from '@mui/joy/ListDivider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Home from '@mui/icons-material/Home';
+import Typography from '@mui/joy/Typography';
+
+function Header(props) {
+    return (
+        <header>
+            <Typography level="h1" sx={{ textAlign: 'center', mb: '25px' }}>{props.title}</Typography>
+            <Box component="nav" aria-label={props.title} sx={{ flexGrow: 1 }} >
+                <List role="menubar" orientation="horizontal" variant="outlined">
+                    <ListItem role="none">
+                        <ListItemButton role="menuitem" component="a" href="/" aria-label="Domov">
+                            <Home />
+                        </ListItemButton>
+                    </ListItem>
+                    <UserContext.Consumer>
+                        {context => (
+                            context.user ?
+                                <>
+                                    <ListDivider />
+                                    <ListItem role="none">
+                                        <ListItemButton role="menuitem" component="a" href="/profile">
+                                            Profile
+                                        </ListItemButton>
+                                    </ListItem>
+                                    <ListItem role="none" sx={{ marginInlineStart: 'auto' }}>
+                                        <ListItemButton role="menuitem" component="a" href="/logout">
+                                            Logout
+                                        </ListItemButton>
+                                    </ListItem>
+                                </>
+                            :
+                                <>
+                                    <ListItem role="none" sx={{ marginInlineStart: 'auto' }}>
+                                        <ListItemButton role="menuitem" component="a" href="/login">
+                                            Login
+                                        </ListItemButton>
+                                    </ListItem>
+                                    <ListDivider/>
+                                    <ListItem role="none">
+                                        <ListItemButton role="menuitem" component="a" href="/register">
+                                            Register
+                                        </ListItemButton>
+                                    </ListItem>
+                                </>
+                        )}
+                    </UserContext.Consumer>
+                </List>
+            </Box>
+        </header >
+    );
+}
+
+export default Header;
