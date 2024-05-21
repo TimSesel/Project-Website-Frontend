17adac86160de4d001335145b18b34ceff2b9c6b
TimSesel <tim.sesel@student.um.si>
2024-05-21 13:17:50 +0200

    PROJECT-15 Dark mode + Tabs
    Switched to dark mode.
    
    Added tabs to represent different dates of captured data

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from "./components/Profile";
 import Logout from "./components/Logout";
 import Map from "./components/Map";
 import Box from '@mui/joy/Box';
+import { CssVarsProvider, ThemeProvider, useTheme } from '@mui/joy/styles';
 
 function App() {
   const [user, setUser] = useState(localStorage.user ? JSON.parse(localStorage.user) : null);
@@ -16,26 +17,32 @@ function App() {
     setUser(userInfo);
   }
 
+  const theme = useTheme();
+
   return (
-    <BrowserRouter>
-      <UserContext.Provider value={{
-        user: user,
-        setUserContext: updateUserData
-      }}>
-        <div className="App">
-          <Header title="NOISE POLLUTION"></Header>
-          <Box component="section" sx={{ m: '2%', p: 2, bgcolor: 'neutral.100', borderRadius: 16 }}>
-            <Routes>
-              <Route path="/" exact element={<Map />}></Route>
-              <Route path="/login" exact element={<Login />}></Route>
-              <Route path="/register" element={<Register />}></Route>
-              <Route path="/profile" element={<Profile />}></Route>
-              <Route path="/logout" element={<Logout />}></Route>
-            </Routes>
-          </Box>
-        </div>
-      </UserContext.Provider>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssVarsProvider defaultMode="dark">
+        <BrowserRouter>
+          <UserContext.Provider value={{
+            user: user,
+            setUserContext: updateUserData
+          }}>
+            <div className='App'>
+              <Header title="NOISE POLLUTION"></Header>
+              <Box component="section" sx={{ m: '2%', p: 2, bgcolor: 'background.level1', borderRadius: 16 }}>
+                <Routes>
+                  <Route path="/" exact element={<Map />}></Route>
+                  <Route path="/login" exact element={<Login />}></Route>
+                  <Route path="/register" element={<Register />}></Route>
+                  <Route path="/profile" element={<Profile />}></Route>
+                  <Route path="/logout" element={<Logout />}></Route>
+                </Routes>
+              </Box>
+            </div>
+          </UserContext.Provider>
+        </BrowserRouter>
+      </CssVarsProvider>
+    </ThemeProvider>
   );
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,43 @@
+import * as React from 'react';
+import Tabs from '@mui/joy/Tabs';
+import TabList from '@mui/joy/TabList';
+import Tab, { tabClasses } from '@mui/joy/Tab';
+import TabPanel from '@mui/joy/TabPanel';
+
 function Map(){
     return (
-        <div>Map</div>
+        <Tabs aria-label="Dates" defaultValue={0} sx={{ borderBottomLeftRadius: 16, borderBottomRightRadius: 16 }}>
+            <TabList sx={{
+                p: 1,
+                justifyContent: 'center',
+                [`&& .${tabClasses.root}`]: {
+                    flex: 'initial',
+                    bgcolor: 'transparent', '&:hover': {bgcolor: 'background.level1',},
+                    [`&.${tabClasses.selected}`]: {
+                        color: 'primary.plainColor', '&::after': {
+                            height: 2,
+                            borderTopLeftRadius: 16,
+                            borderTopRightRadius: 16,
+                            bgcolor: 'primary.500',
+                        },
+                    },
+                },
+            }}
+            >
+                <Tab>First tab</Tab>
+                <Tab>Second tab</Tab>
+                <Tab>Third tab</Tab>
+            </TabList>
+            <TabPanel value={0} sx={{width: '100%'}}>
+                <b>First</b> tab panel
+            </TabPanel>
+            <TabPanel value={1}>
+                <b>Second</b> tab panel
+            </TabPanel>
+            <TabPanel value={2}>
+                <b>Third</b> tab panel
+            </TabPanel>
+        </Tabs>
     );
 }
 
