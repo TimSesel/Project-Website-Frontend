b8dc29b543e12a45b6b8ea666d4a91c1d4d6ff8d
tristan-gajsek <tristan.gajsek@student.um.si>
2024-05-27 14:07:50 +0200

    Add a form to the Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,41 @@
-function Login(){
-    return (
-        <div>Login</div>
-    );
+import { useContext, useState } from "react";
+import { UserContext } from "../userContext";
+import { Navigate } from "react-router-dom";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const userContext = useContext(UserContext);
+
+  async function logIn(e) {}
+
+  return (
+    <div style={{ width: "50%" }}>
+      <form onSubmit={logIn}>
+        {userContext.user ? <Navigate replace to="/" /> : ""}
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="submit"
+          name="submit"
+          value="Log in"
+          style={{ margin: "auto", display: "block" }}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
