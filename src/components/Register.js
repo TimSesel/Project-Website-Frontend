d7e3063de52278688fe4f08ab825fefdd56c1b94
tristan-gajsek <tristan.gajsek@student.um.si>
2024-05-27 14:08:09 +0200

    Add a form to the Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,45 @@
-function Register(){
-    return (
-        <div>Register</div>
-    );
+import { useState } from "react";
+
+function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function Register(e) {}
+
+  return (
+    <div style={{ width: "50%" }}>
+      <form onSubmit={Register}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="submit"
+          name="submit"
+          value="Login"
+          style={{ margin: "auto", display: "block" }}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
