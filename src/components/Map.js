8f51c498f73548c16a62a55623c218ae7c6a4995
tristan-gajsek <tristan.gajsek@student.um.si>
2024-05-27 15:29:24 +0200

    Add a map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,22 @@
-function Map(){
-    return (
-        <div>Map</div>
-    );
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function Map() {
+  const position = [51.505, -0.09];
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
-export default Map
+export default Map;
