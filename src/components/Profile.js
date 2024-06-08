076aa6e67e9af77ec8870fa3c162e0c46428c7c3
dynamo <zigagorenc1@gmail.com>
2024-06-08 14:09:12 +0200

    Updated the look of user profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "../userContext";
+import Typography from "@mui/joy/Typography";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Button from "@mui/joy/Button";
+
 function Profile(){
+    const userContext = useContext(UserContext);
+    const { user } = userContext;
+
     return (
-        <div>Profile</div>
+        <Card>
+            <Typography level="h3">
+                Profile
+            </Typography>
+            <CardContent>
+                <Typography level="body2">
+                    Username: {user.username}
+                </Typography>
+                <Typography level="body2">
+                    Email: {user.email}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 
-export default Profile
+export default Profile;
